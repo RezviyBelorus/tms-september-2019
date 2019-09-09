1647c49b8f592eaf143288edc1ec907bd9c86897
Alexander Fomin <rezviybelorus@gmail.com>
2019-09-09 18:00:30 +0300

    lec_2 tasks

--- src/lec_2/prepare/Main.java
+++ src/lec_2/prepare/Main.java
@@ -1,5 +1,7 @@
 package lec_2.prepare;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         double d = 1.4;
@@ -15,7 +17,18 @@ public class Main {
 
         String x = "123";
 //        Integer s = (Integer) x;
+
+        System.out.println(2<<2);
+        System.out.println(16>>2);
+
+        char myChar = 33;
+        char sym = '\uffff';
+        char symB = 'K';
+        System.out.println(myChar);
+        System.out.println(sym);
+        System.out.println(symB);
     }
+
 
     public static void incrementByOne(int x) {
         System.out.println(x++);
