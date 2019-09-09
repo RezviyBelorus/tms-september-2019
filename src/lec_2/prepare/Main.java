81831acf09c92318ee666cb0e88209ad81445647
AleksandrFomin <RezviyBelorus@gmail.com>
2019-09-09 09:31:46 +0300

    lec_2

---
+++ src/lec_2/prepare/Main.java
@@ -0,0 +1,23 @@
+package lec_2.prepare;
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 1.4;
+        double c = 1.7;
+        System.out.println(d+c);
+
+//        Integer x = null;
+//
+//        incrementByOne(x);
+
+        System.out.println(8<<3);
+        System.out.println(8>>50);
+
+        String x = "123";
+//        Integer s = (Integer) x;
+    }
+
+    public static void incrementByOne(int x) {
+        System.out.println(x++);
+    }
+}
